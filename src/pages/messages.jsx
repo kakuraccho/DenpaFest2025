ee6216dfac5de1b3618d03bb09a7ee2273b7f1e0
kakuraccho <kakurattyo1216@gmail.com>
2025-10-16 17:23:05 +0900

    make committee message

--- src/pages/messages.jsx
+++ src/pages/messages.jsx
@@ -1,11 +1,13 @@
 import * as React from "react"
 import Body from "../components/body"
 import PrincipalMessage from "../contents/MessagePrincipal"
+import CommitteeMessage from "../contents/MessageCommittee"
 
 export default function Messages() {
     return (
         <Body>
             <PrincipalMessage />
+            <CommitteeMessage />
         </Body>
     )
 }
